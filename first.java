e7378ebcd42ceb4ff7cdf7507e53c45b6bccd0fc
24AG1A05I9 <akashvadithya19@gmail.com>
2025-09-18 19:31:37 +0530

    Add files via upload

---
+++ first.java
@@ -0,0 +1,8 @@
+public class first {
+    public static void main(String... args){
+        System.out.print("shiva\n");
+        System.out.println("ace coolege");
+        System.out.println("Iam learning java from begining");
+    }
+    
+}
