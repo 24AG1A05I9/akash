447aaf77d2cf5af0f474146937ffe1ad7b656650
24AG1A05I9 <akashvadithya19@gmail.com>
2025-09-18 19:31:56 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,26 @@
+import java.util.*;
+/*public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);                      // taking input from the user
+        String b = sc.next();                             // next states that whatever next string print in the next input form
+                                                       //.next will print till before spaces only and space after will not be printed;
+
+        System.out.println(b);                       // here b values stores in the .next and then name is giving to the b it wii prints
+    }
+    
+}
+    */
+
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       // String a = sc.nextLine();
+        //System.out.println(a);
+
+        int a = sc.nextInt();           // nextInt --> taking the integer input;
+        System.out.println(a);
+
+    }
+}
+
