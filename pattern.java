c2934e1e8b09ecc633542e9a160afeb7059bda20
24AG1A05I9 <akashvadithya19@gmail.com>
2025-09-18 19:32:21 +0530

    Add files via upload

---
+++ pattern.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+/*public class pattern {
+   // public static void main(String[] args) {
+     //   Scanner s = new Scanner(System.in);
+       // int n = s.nextInt();
+        //for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.println(j);
+            }
+
+        }
+        System.out.println("\n");
+    }
+    
+}
+*/
+public class pattern{
+    public static void main(String[] args){
+        System.out.println("* * * * ");
+        System.out.println("* * * ");
+        System.out.println("* *  ");
+        System.out.println("*  ");
+    }
+}
