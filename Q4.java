432950c7dabc13bc9057b38de6168f8818c726f1
24AG1A05I9 <akashvadithya19@gmail.com>
2025-09-20 00:39:59 +0530

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,25 @@
+public class Q4 {
+
+    public static void main(String[] args){
+       /*  int x = 2,y=5;
+        int a = (x *y /x);
+        int a1 = (x * (y/x));
+        System.out.print(a + " ,");
+        System.out.print(a1);
+
+        */
+
+
+        int b = 200,c =50,d=100;
+        if(b>c && c>d){
+            System.out.println("akash");
+        }
+        if(c>b && c>d ){
+            System.out.println("java");
+        }
+        if((c+200) < b && (c+150)<d){
+            System.out.println("akash java");
+        }
+    }
+    
+}
