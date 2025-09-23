00b50c60ec4ef2156a5c92ad9bdb43e60a22ef66
24AG1A05I9 <akashvadithya19@gmail.com>
2025-09-23 22:00:17 +0530

    Add files via upload

---
+++ DAY02/ternnary.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ternnary{
+	public static void main(String...args){
+		int number = 4;
+		String ap = (number % 2==0) ? "even" : "odd";
+		System.out.println(ap);
+		int type = (number <=0) ? 4: 90;
+		System.out.println(type);
+		int numbers = 45;
+		String akash = (numbers >=33) ? "pass" : "fail";
+		System.out.println("aksh is passes in the semester of 1...2");
+
+	}
+}
