85672051db3ad1ebae35153af3d08cb3d8049358
24AG1A05I9 <akashvadithya19@gmail.com>
2025-09-23 22:41:15 +0530

    Add files via upload

---
+++ DAY02/Q31.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Q31{
+	public static void main(String...args){
+		Scanner sc = new Scanner(System.in);
+		int number = sc.nextInt();
+		if(number >= 0){
+
+
+		System.out.println("the number is may be postive");
+	}
+	else{
+		System.out.println("the number is negative");
+
+	}
+
+
+		double temp = 103.5;
+		if(temp > 100 ){
+			System.out.println("you have fewer akash");
+		}
+		else{
+			System.out.println("You dont have fewer aksh");
+		}
+
+
+			}
+}
