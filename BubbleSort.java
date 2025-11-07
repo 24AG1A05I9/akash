590a4fda7a3eafbf2669324fdcab74025dff0af4
AKASH VADIHTYA <akashvadithya19@gmail.com>
2025-11-07 22:04:37 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+  public class BubbleSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        sc.close();
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no elements were swapped, array is sorted
+            if (!swapped) break;
+        }
+    }
+}
+
+
+
+
+
+
+// bubble sorting implemetation
+/*class BubbleSort{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println(arr);
+    }
+}
+*/
---
+++ FibonaciiSeries.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class FibonaciiSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       // int n = 10; // number of terms
+        int first = 0, second = 1;
+
+        System.out.print("Fibonacci Series: " + first + " " + second);
+
+        for (int i = 2; i < n; i++) {
+            int next = first + second;
+            System.out.print(" " + next);
+            first = second;
+            second = next;
+        }
+    }
+}
